1b19f34b71603e923a755bedf23a535f02171b72
shashi-kant10 <shashiofficial10@gmail.com>
2022-07-14 00:49:48 +0530

    Added Volley library and API

---
+++ app/src/main/java/com/shashi/newsdozz/Constants.kt
@@ -0,0 +1,14 @@
+package com.shashi.newsdozz
+
+class Constants {
+
+    companion object {
+
+        const val NEWS_API =
+            "https://gnews.io/api/v4/top-headlines?lang=en&country=in&topic=breaking-news&token=ef098601144aaa99d14f8cd6d85eb7d8"
+
+        const val TAG = "newsdozz"
+
+    }
+
+}
--- app/src/main/java/com/shashi/newsdozz/HomeActivity.kt
+++ app/src/main/java/com/shashi/newsdozz/HomeActivity.kt
@@ -1,8 +1,19 @@
 package com.shashi.newsdozz
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.shashi.newsdozz.databinding.ActivityHomeBinding
+import kotlin.math.log
 
 class HomeActivity : AppCompatActivity() {
 
@@ -15,5 +26,45 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        loadNews()
     }
+
+    private fun loadNews() {
+        val url = Constants.NEWS_API
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+
+                // Get url of image
+                var articleResponse = response.getJSONArray("articles")
+                var artcleNews = ArrayList<NewsData>()
+
+                for (i in 0 until articleResponse.length()) {
+
+                    var newsJsonObject = articleResponse.getJSONObject(i)
+
+                    var news = NewsData(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("description"),
+                        newsJsonObject.getString("image"),
+                        newsJsonObject.getString("url")
+                    )
+
+                    Log.d(Constants.TAG, "loadNews: ${news.title}")
+
+                }
+
+
+            },
+            { error ->
+                // TODO: Handle error
+            }
+        )
+
+        VolleySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+
+    }
+
 }
+
