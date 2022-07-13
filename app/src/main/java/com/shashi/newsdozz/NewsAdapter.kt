20e889d5b28b06d887cddd09843d1d023c8eda2a
shashi-kant10 <shashiofficial10@gmail.com>
2022-07-14 00:11:58 +0530

    Added News Adapter and News data class

---
+++ app/src/main/java/com/shashi/newsdozz/NewsAdapter.kt
@@ -0,0 +1,43 @@
+package com.shashi.newsdozz
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
+
+class NewsAdapter(var context: Context, var newsList: ArrayList<NewsData>) :
+    RecyclerView.Adapter<NewsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        var view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_layout_news, parent, false)
+
+        view.setOnClickListener {
+            // TODO: Implement on click
+        }
+
+        return NewsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+
+        holder.tvNewsTitle.text = newsList.get(position).title
+
+    }
+
+    override fun getItemCount(): Int {
+        return newsList.size
+    }
+
+}
+
+class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    var ivNewsImage = itemView.findViewById<ImageView>(R.id.ivNewsIVN)
+    var tvNewsTitle = itemView.findViewById<TextView>(R.id.tvNewsTitleIV)
+    var tvNewsDesc = itemView.findViewById<TextView>(R.id.tvNewsDescIV)
+
+}
---
+++ app/src/main/java/com/shashi/newsdozz/NewsData.kt
@@ -0,0 +1,11 @@
+package com.shashi.newsdozz
+
+import android.net.Uri
+
+data class NewsData(
+    var uri: String = "",
+    var title: String = "",
+    var desc: String = ""
+) {
+
+}
